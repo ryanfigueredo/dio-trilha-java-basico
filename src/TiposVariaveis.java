12f982918a8a67e258788dda49f63ab0d24f22b7
Ryan <ryan.figueredo059@gmail.com>
2024-05-03 12:09:39 -0300

    Variáveis e Constantes

--- src/TiposVariaveis.java
+++
@@ -1,5 +0,0 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
-    }
-}
